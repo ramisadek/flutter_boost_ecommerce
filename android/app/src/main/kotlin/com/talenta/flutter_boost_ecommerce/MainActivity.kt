aed730be11dab0b8fadab822b7f9306567168805
rami <ramyahmedsadek@gmail.com>
2020-12-16 17:44:42 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/talenta/flutter_boost_ecommerce/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.talenta.flutter_boost_ecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
